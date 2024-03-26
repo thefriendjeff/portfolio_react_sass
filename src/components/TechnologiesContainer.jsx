3e6d87dfad3c2a1b7e3fc15b9eb0b1691950ff79
thefriendjeff <jeffersonsena.jw@gmail.com>
2024-03-26 20:20:42 -0300

    Adicionando tecnologias

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -4,23 +4,30 @@ import {
     DiJsBadge,
     DiReact,
     DiBootstrap,
-    DiSass
+    DiSass,
+    DiPhotoshop 
 } from "react-icons/di";
+
+import {
+    PiFigmaLogoThin  
+} from "react-icons/pi";
 
 import "../style/components/technologiescontainer.sass";
 
 const technologies = [
-    {id: "html", name:"HTML5", icon: <DiHtml5 />, knowledge: "Conhecimento em estruturação de páginas web."},
+    {id: "html", name:"Html5", icon: <DiHtml5 />, knowledge: "Conhecimento em estruturação de páginas web."},
     {id: "css", name:"CSS3", icon: <DiCss3 />, knowledge: "Estilização avançada com CSS, incluindo flexbox e grid."},
     {id: "js", name:"JavaScript", icon: <DiJsBadge />, knowledge: "Domínio de ES6+, manipulação do DOM e assincronismo."},
-    {id: "react", name:"REACT", icon: <DiReact />, knowledge: "Criação de SPA com React, uso de hooks e context API."},
-    {id: "bootstrap", name:"BOOTSTRAP", icon: <DiBootstrap />, knowledge: "Desenvolvimento rápido de layouts responsivos."},
+    {id: "react", name:"React", icon: <DiReact />, knowledge: "Criação de SPA com React, uso de hooks e context API."},
+    {id: "bootstrap", name:"Bootstrap", icon: <DiBootstrap />, knowledge: "Desenvolvimento rápido de layouts responsivos."},
     {id: "sass", name:"SASS", icon: <DiSass />, knowledge: "Integração com react para estilização dinâmica."},
+    {id: "figma", name:"Figma", icon: <PiFigmaLogoThin />, knowledge: "Criação de Layouts e manipulação de ferramentas de edição"},
+    {id: "photoshop", name:"Photoshop", icon: <DiPhotoshop />, knowledge: "Manipulação e edição de imagem, criação de letreiros 3d"},
 ];
 
 const TechnologiesContainer = () => {
     return <section className="technologies-container">
-        <h2>Tecnologias</h2>
+        <h2>Habilidades</h2>
         <div className="technologies-grid">
             {technologies.map((tech) => (
                 <div className="technology-card" id={tech.id} key={tech.id}>
@@ -29,7 +36,7 @@ const TechnologiesContainer = () => {
                         <h3>{tech.name}</h3>
                         <p>{tech.knowledge}</p>
                     </div>
-                </div>
+                </div> 
             ))}
         </div>
     </section>;
